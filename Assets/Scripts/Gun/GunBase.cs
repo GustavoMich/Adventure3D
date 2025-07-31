f12326843f93217449380afb99d8342c39e7ce9c
GustavoMich <gustavomichalski5@gmail.com>
2025-07-31 19:53:08 -0300

    shoot limits
    adicionado limite de tiros para a arma;

--- Assets/Scripts/Gun/GunBase.cs
+++ Assets/Scripts/Gun/GunBase.cs
@@ -17,7 +17,7 @@ public class GunBase : MonoBehaviour
     private Coroutine _currentCoroutine;
 
 
-    IEnumerator ShootCoroutine()
+    protected virtual IEnumerator ShootCoroutine()
     {
         while (true)
         {
