a299f105b43c2f5fdc4c25cdb4a1ed8aa5714f93
GustavoMich <gustavomichalski5@gmail.com>
2025-07-31 21:10:52 -0300

    ammo ui
    adicionado UI para as balas da arma do jogador.

--- Assets/Scripts/Gun/GunShootLimits.cs
+++ Assets/Scripts/Gun/GunShootLimits.cs
@@ -1,14 +1,23 @@
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GunShootLimits : GunBase
 {
+    public List<UIGunUpdater> uIGunUpdaters;
+
+
     public float maxShoot = 5f;
     public float timeToRecharge = 1f;
 
     private float _currentShoots;
     private bool _recharging = false;
+
+    private void Awake()
+    {
+        GetAllUIs();
+    }
 
     protected override IEnumerator ShootCoroutine()
     {
@@ -21,6 +30,7 @@ public class GunShootLimits : GunBase
                 Shoot();
                 _currentShoots++;
                 CheckRecharge();
+                UpdateUI();
                 yield return new WaitForSeconds(timeBetweenShoot);
             }
         }
@@ -47,10 +57,20 @@ public class GunShootLimits : GunBase
         while(time < timeToRecharge)
         {
             time += Time.deltaTime;
-            Debug.Log("Rechargin: " + time);
+            uIGunUpdaters.ForEach(i => i.UpdateValue(time/timeToRecharge));
             yield return new WaitForEndOfFrame();
         }
         _currentShoots = 0;
         _recharging = false;
+    }
+
+    private void UpdateUI()
+    {
+        uIGunUpdaters.ForEach(i => i.UpdateValue(maxShoot, _currentShoots));
+    }
+
+    private void GetAllUIs()
+    {
+        uIGunUpdaters = GameObject.FindObjectsOfType<UIGunUpdater>().ToList();
     }
 }
