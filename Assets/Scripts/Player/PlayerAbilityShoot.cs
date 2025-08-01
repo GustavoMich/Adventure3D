722a711e9b9cd1a13e7b8be74f80c93190ea4627
GustavoMich <gustavomichalski5@gmail.com>
2025-07-31 22:14:55 -0300

    change weapons
    adicionado troca de armas ao jogador nas teclas 1 e 2.

--- Assets/Scripts/Player/PlayerAbilityShoot.cs
+++ Assets/Scripts/Player/PlayerAbilityShoot.cs
@@ -8,27 +8,49 @@ public class PlayerAbilityShoot : PlayerAbilityBase
   
 
     public GunBase gunBase;
+    public GunBase secondGunBase;
     public Transform gunPosition;
 
     private GunBase _currentGun;
+
 
     protected override void Init()
     {
         base.Init();
 
         CreateGun();
+        
+       
 
         inputs.Gameplay.Shoot.performed += ctx => StartShoot();
         inputs.Gameplay.Shoot.canceled += ctx => CancelShoot();
 
+        inputs.Gameplay.ChangeGun1.performed += ctx => CreateGun();
+        inputs.Gameplay.ChangeGun2.performed += ctx => CreateSecondGun();
+
     }
+
 
     private void CreateGun()
     {
+        if (_currentGun != null)
+            Destroy(_currentGun.gameObject);
+
         _currentGun = Instantiate(gunBase, gunPosition);
 
         _currentGun.transform.localPosition = _currentGun.transform.localEulerAngles = Vector3.zero;
     }
+
+    private void CreateSecondGun()
+    {
+        if (_currentGun != null)
+            Destroy(_currentGun.gameObject);
+
+        _currentGun = Instantiate(secondGunBase, gunPosition);
+
+        _currentGun.transform.localPosition = _currentGun.transform.localEulerAngles = Vector3.zero;
+    }
+
 
     private void StartShoot()
     {
