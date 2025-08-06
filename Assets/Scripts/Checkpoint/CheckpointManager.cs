2300dc7620be0008d3599274ae270ab2f4a9ed9a
GustavoMich <gustavomichalski5@gmail.com>
2025-08-06 16:01:20 -0300

    checkpoint text and more
    foi adicionado uma barra de vida em cima do jogador e no canto inferior direito da tela, e um texto de checkpoint ativado ao ativar um check.

--- Assets/Scripts/Checkpoint/CheckpointManager.cs
+++ Assets/Scripts/Checkpoint/CheckpointManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Ebac.Core.Singleton;
 
 public class CheckpointManager : Singleton<CheckpointManager>
@@ -8,6 +9,21 @@ public class CheckpointManager : Singleton<CheckpointManager>
     public int lastCheckPointKey = 0;
 
     public List<CheckpointBase> checkpoints;
+
+    [Header("UI Checkpoint")]
+    public Text checkpointText;
+
+    public float textDuration = 3f; 
+
+    private Coroutine textCoroutine;
+
+    private void Start()
+    {
+        if (checkpointText != null)
+            checkpointText.gameObject.SetActive(false);
+    }
+
+
 
     public bool HasCheckpoint()
     {
@@ -20,6 +36,7 @@ public class CheckpointManager : Singleton<CheckpointManager>
         if(i > lastCheckPointKey)
         {
             lastCheckPointKey = i;
+            ShowCheckpointText();
         }
     }
 
@@ -27,5 +44,25 @@ public class CheckpointManager : Singleton<CheckpointManager>
     {
        var checkpoint = checkpoints.Find(i => i.key == lastCheckPointKey);
         return checkpoint.transform.position;
+    }
+
+
+    private void ShowCheckpointText()
+    {
+        if (checkpointText == null) return;
+
+        
+        if (textCoroutine != null)
+            StopCoroutine(textCoroutine);
+
+        checkpointText.gameObject.SetActive(true);
+
+        textCoroutine = StartCoroutine(HideCheckpointText());
+    }
+
+    private IEnumerator HideCheckpointText()
+    {
+        yield return new WaitForSeconds(textDuration);
+        checkpointText.gameObject.SetActive(false);
     }
 }
