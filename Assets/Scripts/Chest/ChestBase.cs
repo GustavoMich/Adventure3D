52a151fa2f406064e4a705cbaeb03f90bd2e3aa5
GustavoMich <gustavomichalski5@gmail.com>
2025-08-09 15:57:41 -0300

    chest coin
    adicionado baú que ao abrir da moedas ao jogador.

---
+++ Assets/Scripts/Chest/ChestBase.cs
@@ -0,0 +1,89 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestBase : MonoBehaviour
+{
+    public KeyCode keyCode = KeyCode.Z;
+    public Animator animator;
+    public string triggerOpen = "Open";
+
+    [Header("Notification")]
+    public GameObject notification;
+    public float tweenDuration = .2f;
+    public Ease TweenEase = Ease.OutBack;
+
+    [Space]
+    public ChestItemBase chestItem;
+
+
+    private float startScale;
+
+    private bool _chestOpened = false;
+
+    void Start()
+    {
+        startScale = notification.transform.localScale.x;
+        HideNotification();
+    }
+
+    private void OpenChest() 
+    {
+        if (_chestOpened) return;
+
+        animator.SetTrigger(triggerOpen);
+        _chestOpened = true;
+        HideNotification();
+        Invoke(nameof(ShowItem), 1f);
+    }
+
+    private void ShowItem()
+    {
+        chestItem.ShowItem();
+        Invoke(nameof(CollectItem), 1f);
+    }
+
+    private void CollectItem()
+    {
+        chestItem.Collect();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        PlayerController p = other.transform.GetComponent<PlayerController>();
+        if(p != null)
+        {
+            ShowNotification();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        PlayerController p = other.transform.GetComponent<PlayerController>();
+        if (p != null)
+        {
+            HideNotification();
+        }
+    }
+
+    private void ShowNotification()
+    {
+        notification.SetActive(true);
+        notification.transform.localScale = Vector3.zero;
+        notification.transform.DOScale(startScale, tweenDuration);
+    }
+
+    private void HideNotification()
+    {
+        notification.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(keyCode) && notification.activeSelf)
+        {
+            OpenChest();
+        }
+    }
+}
