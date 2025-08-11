883ce69ea827ca682066ae36e2541177061f8cdb
GustavoMich <gustavomichalski5@gmail.com>
2025-08-11 09:50:16 -0300

    missed

--- Assets/Scripts/Utils/PlayerMagneticTrigger.cs
+++ Assets/Scripts/Utils/PlayerMagneticTrigger.cs
@@ -5,10 +5,11 @@ using Itens;
 
 public class PlayerMagneticTrigger : MonoBehaviour
 {
-    
 
     private void OnTriggerEnter(Collider other)
     {
+        
+
         ItemCollectableBase i = other.transform.GetComponent<ItemCollectableBase>();
         if (i != null)
         {
