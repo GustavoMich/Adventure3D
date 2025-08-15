b50379b18c0557d104adbc44ec3fccf8980e2f14
GustavoMich <gustavomichalski5@gmail.com>
2025-08-15 15:16:20 -0300

    new stage

--- Assets/Scripts/SaveManager/SaveManager.cs
+++ Assets/Scripts/SaveManager/SaveManager.cs
@@ -2,9 +2,22 @@ using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Ebac.Core.Singleton;
 
-public class SaveManager : MonoBehaviour
+public class SaveManager : Singleton<SaveManager>
 {
+    private SaveSetup _saveSetup;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        _saveSetup = new SaveSetup();
+        _saveSetup.lastLevel = 2;
+        _saveSetup.playerName = "Test";
+    }
+
+
+    #region SAVE
     private void Save()
     {
         SaveSetup setup = new SaveSetup();
@@ -16,12 +29,36 @@ public class SaveManager : MonoBehaviour
         SaveFile(setupToJson);
     }
 
+    public void SaveName(string text)
+    {
+        _saveSetup.playerName = text;
+        Save();
+    }
+
+    public void SaveLastLevel(int level)
+    {
+        _saveSetup.lastLevel = level;
+        Save();
+    }
+    #endregion
+
+
     private void SaveFile(string json)
     {
         string path = Application.streamingAssetsPath + "/save.txt";
 
         Debug.Log(path);
         File.WriteAllText(path, json);
+    }
+
+    private void SaveLevelOne()
+    {
+        SaveLastLevel(1);
+    }
+
+    private void SaveLevelFive()
+    {
+        SaveLastLevel(5);
     }
 }
 
