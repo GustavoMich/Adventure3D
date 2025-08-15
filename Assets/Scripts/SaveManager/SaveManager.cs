bfb2209cf755010c96ad6d15b1143262a2473773
GustavoMich <gustavomichalski5@gmail.com>
2025-08-15 14:33:46 -0300

    save manager
    foi adicionado um save manager no novo menu.

---
+++ Assets/Scripts/SaveManager/SaveManager.cs
@@ -0,0 +1,33 @@
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveManager : MonoBehaviour
+{
+    private void Save()
+    {
+        SaveSetup setup = new SaveSetup();
+        setup.lastLevel = 2;
+        setup.playerName = "Test";
+
+        string setupToJson = JsonUtility.ToJson(setup, true);
+        Debug.Log(setupToJson);
+        SaveFile(setupToJson);
+    }
+
+    private void SaveFile(string json)
+    {
+        string path = Application.streamingAssetsPath + "/save.txt";
+
+        Debug.Log(path);
+        File.WriteAllText(path, json);
+    }
+}
+
+[System.Serializable]
+public class SaveSetup 
+{
+    public int lastLevel;
+    public string playerName;
+}
